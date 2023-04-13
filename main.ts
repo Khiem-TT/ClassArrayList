11c879dc3f54d2858fd38d3e0bba81122ec0c8b2
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-04-13 10:02:46 +0700

    first commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+import {ArrayList} from "./ArrayList";
+
+interface Post {
+    title: string;
+}
+
+let arrayList = new ArrayList<Post>();
+arrayList.add({title: 'Lập trình JS'});
+arrayList.add({title: 'Lập trình PHP'});
+arrayList.add({title: 'Lập trình Java'});
+console.log(arrayList.container);
+
+console.log(arrayList.get(1));
+
+arrayList.remove();
+console.log(arrayList.container);
+
+console.log(arrayList.size());
